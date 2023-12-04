c5c136e2f2cbca1cef98a49cc228d1dc00abac05
PhaneeChowdary <phaneendrababugunturu4393@gmail.com>
2023-12-04 13:39:27 -0500

    PLRNN

--- basicPLRNN.py
+++ basicPLRNN.py
@@ -10,6 +10,7 @@ from matplotlib import pyplot as plt
 from flax.linen import RNNCellBase, compact
 
 
+# Rossler dynamical system, used for generating synthetic data.
 def rossler(u, t, p):
     x, y, z = u
     a, b, c = p
@@ -22,6 +23,7 @@ def rossler(u, t, p):
 good_u0 = np.array([-4.53741665, 2.17898447, 0.02006643])
 
 
+# Generates data based on the Rossler system.
 def get_data(tt, a=0.2, b=0.2, c=5.7, u0=good_u0):
     p = np.array([a, b, c])
     sol = integrate.odeint(rossler, u0, tt, args=(p,))
@@ -29,10 +31,29 @@ def get_data(tt, a=0.2, b=0.2, c=5.7, u0=good_u0):
 
 
 class basicPLRNNCell(RNNCellBase):
+
+    """
+    Implementation of Piecewise Linear Recurrent Neural Network (PLRNN) using JAX
+
+    Latent Model (z_t = A*z_{t-1} + W*phi(z_{t-1}) + C*s_t):
+
+    z_t: The latent state at time t.
+    A: A diagonal matrix that influences the latent state from the previous time step (z_{t-1}).
+    W: An off-diagonal weight matrix applied to the nonlinear function phi, which is a ReLU (max(0, z)).
+    C: A matrix that incorporates the external input s_t into the latent state.
+    phi: A non-linear function (ReLU) applied to the latent state.
+
+    Observation Model (x_t = B*z_t):
+
+    x_t: The observed output at time t.
+    B: A matrix that maps the latent state to the observed space.
+    """
+
     D: int  # Number of variables in latent space
     N: int  # Number of observed neuronal firing rates
     dtype: Any = jnp.float32
 
+    # Create matrix A (Diagonal Matrix)
     def init_A(self, key, shape, dtype):
         D = shape[0]
         scale = 10
@@ -43,6 +64,7 @@ class basicPLRNNCell(RNNCellBase):
         A = jnp.diag(H)
         return A
 
+    # Create matrix W (Off-Diagonal Matrix)
     def init_W(self, key, shape, dtype):
         D = shape[0]
         scale = 10
@@ -54,6 +76,7 @@ class basicPLRNNCell(RNNCellBase):
         W = H - A*jnp.eye(D)
         return W
 
+    # Initialize A, W, C, and B
     def setup(self):
         # Diagonal matrix A
         self.A = Dense(1, use_bias=False, dtype=self.dtype,
@@ -69,6 +92,7 @@ class basicPLRNNCell(RNNCellBase):
         # B Matrix
         self.B = Dense(self.N, use_bias=False, dtype=self.dtype)
 
+    # Forward pass
     @compact
     def __call__(self, carry, inputs):
         z_prev = carry
@@ -99,6 +123,7 @@ class basicPLRNNCell(RNNCellBase):
         return z_init
 
 
+# Loss function that computes MSE
 @jax.jit
 def compute_loss_(params, carry, s, obs):
     _, x = n1.apply(params, carry, s)
@@ -107,13 +132,10 @@ def compute_loss_(params, carry, s, obs):
     return jnp.mean(e2)
 
 
+# Reset the diagonal elements of W to Zero
 def reset_W_diagonal(params):
     W = params['params']['W']['kernel']
-
-    # Set the diagonal elements to zero
     W = W.at[jnp.diag_indices(W.shape[0])].set(0)
-
-    # Update the parameters dictionary
     params['params']['W']['kernel'] = W
     return params
 
